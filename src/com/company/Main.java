c50a366adcff06415502517cdb6e351c65e2abcd
rvholk <rvholk@multiweb.nl>
2020-11-24 18:41:48 +0100

    several changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,22 @@ public class Main {
     public static void main(String[] args) {
         int x = 0;
         double y = 0.00;
+        double z = 0.00;
 
-        while(x < 1000) {
+        while (x < 100000) {
             int number = Random(1, 99);
-            if (number < 4) {
-                System.out.println("smaller than 4");
+            if (number < 50) {
                 y += 1;
+            }
+            if (number == 99) {
+                z += 1;
             }
             x += 1;
         }
-        System.out.println("Out of a thousand tries total below 4 is: "+ y);
-        double percentage = y / 1000 * 100;
-        double roundOff = Math.round(percentage*100.0)/100.0;
-        System.out.println("Which is a percentage of: "+ roundOff +"%");
+        System.out.println("Out of one hundred thousand tries total below 50 is: " + y);
+        System.out.println("Out of one hundred thousand tries total times 99: " + z);
+        double percentage = y / 100000 * 100;
+        double roundOff = Math.round(percentage * 100.0) / 100.0;
+        System.out.println("Percentage of below 50: " + roundOff + "%");
     }
 }
